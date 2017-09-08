dd47ae9233e35ef51e11a0918e5facb240abdaa1
ccorr85 <chriscorr1985@gmail.com>
2017-09-08 22:12:04 +0100

    Adding null check

--- src/main/java/com/kata/string/calc/StringCalculator.java
+++ src/main/java/com/kata/string/calc/StringCalculator.java
@@ -3,6 +3,10 @@ package com.kata.string.calc;
 public class StringCalculator {
     
     public int generateSum(String numbersString) {
+        if(numbersString == null) {
+            return -1;
+
+        }
         return 0;
     }
 }
--- src/test/java/com/kata/string/calc/StringCalculatorTest.java
+++ src/test/java/com/kata/string/calc/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.kata.string.calc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,10 +8,22 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setup() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void testEmptyStringReturnsZero() {
-        StringCalculator stringCalculator = new StringCalculator();
         int sum = stringCalculator.generateSum("");
         assertEquals("The sum should be 0 as an empty string was used", 0, sum);
+    }
+
+    @Test
+    public void testNullNumbersStillReturnsMinusOne() {
+        int sum = stringCalculator.generateSum(null);
+        assertEquals("The sum should be -1 as a null string was used", -1, sum);
     }
 }
