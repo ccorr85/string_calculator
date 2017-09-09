06dc5e2a55754ee03ab681564b83c81b4afb43aa
ccorr85 <chriscorr1985@gmail.com>
2017-09-09 22:46:55 +0100

    Adding empty string check sooner in method

--- src/main/java/com/kata/string/calc/StringCalculator.java
+++ src/main/java/com/kata/string/calc/StringCalculator.java
@@ -3,26 +3,28 @@ package com.kata.string.calc;
 public class StringCalculator {
 
     private static final String NUMBER_SEPARATOR = ",";
-    
+
     public int generateSum(String numbersString) {
-        if(numbersString == null) {
+        if (numbersString == null) {
             System.out.println("No sum could be generated as the value is null");
             return -1;
-
         }
-        if (numbersString.length() > 0 ) {
-            int sum = 0;
-            String[] numbers = numbersString.split(NUMBER_SEPARATOR);
-            for (String number : numbers) {
-                try {
-                    sum += Integer.valueOf(number.trim());
-                } catch (NumberFormatException e) {
-                    System.out.print(e.getMessage());
-                }
 
+        if (numbersString.length() == 0) {
+            return 0;
+        }
+
+
+        int sum = 0;
+        String[] numbers = numbersString.split(NUMBER_SEPARATOR);
+        for (String number : numbers) {
+            try {
+                sum += Integer.valueOf(number.trim());
+            } catch (NumberFormatException e) {
+                System.out.print(e.getMessage());
             }
-            return sum;
+
         }
-        return 0;
+        return sum;
     }
 }
